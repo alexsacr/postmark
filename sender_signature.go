cf6f0f323313c7f3d3b6ab1251a5eee4a4f8fb54
Alex Philipp <alex@sacrilege.io>
2021-01-19 13:57:19 -0600

    Add sender signatures

--- sender_signature.go
+++ sender_signature.go
@@ -30,6 +30,16 @@ type SenderSignature struct {
 	ID                            int    `json:"ID"`
 }
 
+// SenderSignatureOpts is passed to CreateSenderSignature to create a new
+// sender signature.
+type SenderSignatureOpts struct {
+	FromEmail        string `json:"FromEmail"`
+	Name             string `json:"Name"`
+	ReplyToEmail     string `json:"ReplyToEmail,omitempty"`
+	ReturnPathDomain string `json:"ReturnPathDomain,omitempty"`
+}
+
+// GetSenderSignature returns a SenderSignature from an ID.
 func (c *Client) GetSenderSignature(signatureID string) (*SenderSignature, error) {
 	var ret SenderSignature
 
@@ -45,14 +55,46 @@ func (c *Client) GetSenderSignature(signatureID string) (*SenderSignature, error
 	return &ret, err
 }
 
-func (c *Client) CreateSenderSignature(sendsig SenderSignature) (*SenderSignature, error) {
-	return nil, nil
+// CreateSenderSignature accepts a SenderSignatureOpts struct and returns a
+// SenderSignature struct with the generated ID.
+func (c *Client) CreateSenderSignature(newSig SenderSignatureOpts) (*SenderSignature, error) {
+	var ret SenderSignature
+
+	req := parameters{
+		method:             http.MethodPost,
+		endpoint:           "/senders",
+		expectedStatusCode: http.StatusOK,
+		respTarget:         &ret,
+		token:              c.AccountToken,
+		payload:            newSig,
+	}
+
+	err := c.doRequest(req)
+	return &ret, err
 }
 
-func (c *Client) UpdateSenderSignature(signatureID string, upsendsig SenderSignature) error {
-	return nil
+// UpdateSenderSignature accepts a SenderSignatureOpts struct and signature ID
+// update.  Note that FromEmail cannot be updated.
+func (c *Client) UpdateSenderSignature(signatureID string, upSig SenderSignatureOpts) error {
+	req := parameters{
+		method:             http.MethodPut,
+		endpoint:           fmt.Sprintf("/senders/%s", signatureID),
+		expectedStatusCode: http.StatusOK,
+		token:              c.AccountToken,
+		payload:            upSig,
+	}
+
+	return c.doRequest(req)
 }
 
+// DeleteSenderSignature will delete a signature by ID.
 func (c *Client) DeleteSenderSignature(signatureID string) error {
-	return nil
+	req := parameters{
+		method:             http.MethodDelete,
+		endpoint:           fmt.Sprintf("/senders/%s", signatureID),
+		expectedStatusCode: http.StatusOK,
+		token:              c.AccountToken,
+	}
+
+	return c.doRequest(req)
 }
--- sender_signature_test.go
+++ sender_signature_test.go
@@ -1,9 +1,13 @@
 package postmark
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,18 +21,50 @@ func TestCRUDSenderSignatureOK(t *testing.T) {
 
 	c := NewClient(accountToken, "")
 
-	//timestamp := time.Now().Unix()
+	timestamp := time.Now().Unix()
 
 	// Create
+	sigOpts := SenderSignatureOpts{
+		FromEmail:        fmt.Sprintf("test+%d@example123.com", timestamp),
+		Name:             "foobarbaz",
+		ReplyToEmail:     "test-reply@example123.com",
+		ReturnPathDomain: "pm.example123.com",
+	}
+
+	retSig, err := c.CreateSenderSignature(sigOpts)
+	require.NoError(t, err, "error creating signature")
+	require.NotEmpty(t, retSig, "got empty signature object returned on create")
 
 	// Read
-	readSig, err := c.GetSenderSignature("1540299")
+	readSig, err := c.GetSenderSignature(strconv.Itoa(retSig.ID))
 	require.NoError(t, err, "failed to read signature")
 	require.NotEmpty(t, readSig, "got empty signature on read")
 
+	assert.Equal(t, sigOpts.FromEmail, readSig.EmailAddress, "wrong email on read")
+	assert.Equal(t, sigOpts.Name, readSig.Name, "wrong name on read")
+	assert.Equal(t, sigOpts.ReplyToEmail, readSig.ReplyToEmailAddress, "wrong replyto on read")
+	assert.Equal(t, sigOpts.ReturnPathDomain, readSig.ReturnPathDomain, "wrong return path domain on read")
+
 	// Update
+	upOpts := SenderSignatureOpts{
+		Name:             "foobarbaz-updated",
+		ReplyToEmail:     "test-reply-updated@example123.com",
+		ReturnPathDomain: "pm-updated.example123.com",
+	}
+
+	err = c.UpdateSenderSignature(strconv.Itoa(retSig.ID), upOpts)
+	require.NoError(t, err, "error on update")
 
 	// Read
+	upSig, err := c.GetSenderSignature(strconv.Itoa(retSig.ID))
+	require.NoError(t, err, "failed to read signature")
+	require.NotEmpty(t, upSig, "got empty signature on read")
+
+	assert.Equal(t, upOpts.Name, upSig.Name, "wrong name on read")
+	assert.Equal(t, upOpts.ReplyToEmail, upSig.ReplyToEmailAddress, "wrong replyto on read")
+	assert.Equal(t, upOpts.ReturnPathDomain, upSig.ReturnPathDomain, "wrong return path domain on read")
 
 	// Delete
+	err = c.DeleteSenderSignature(strconv.Itoa(retSig.ID))
+	require.NoError(t, err, "error on deletion")
 }
